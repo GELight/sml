dc1da4edd78353c1a3ac77df0cf1cad810c7b306
Mario Linz <gelight@gmx.de>
2021-04-12 17:03:22 +0200

    Update release script and package version

--- build-release.sh
+++ build-release.sh
@@ -8,8 +8,8 @@ echo "##############################################"
 GIT_RELEASE_VERSION=$(git describe --abbrev=0 --tags)
 GIT_RELEASE_VERSION=$(echo $GIT_RELEASE_VERSION| cut -d'v' -f 2)
 
-echo $RELEASE_VERSION
 echo $GIT_RELEASE_VERSION
+echo $RELEASE_VERSION
 
 if [[ "$RELEASE_VERSION" == "$GIT_RELEASE_VERSION" ]]; then
     echo "Release version $RELEASE_VERSION already exists on git!"
