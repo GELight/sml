8649bc988922138feaf4af8e9d4739ea8f4077d9
Mario Linz <gelight@gmx.de>
2021-04-12 17:29:19 +0200

    Fix the last release 0.1.0 because wrong SmlRequest class committed for server side. The client side SmlRequest class will proveded in sml-client package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,9 @@
 * **SmlRequest class:** provide a new class for client side SML requests
 * **sml:** remove generated technical docs based on markdown
 * **typescript:** rename type definition file from main.d.ts into types.d.ts
+
+## [0.1.1](https://github.com/GELight/sml) (2021-04-12)
+
+### RELEASE FIX - SmlRequest
+
+* **fix:** Fix the last release 0.1.0 because wrong SmlRequest class committed for server side. The client side SmlRequest class will proveded in [sml-client](https://www.npmjs.com/package/@gelight/sml-client) package
--- src/sml/SmlRequest.ts
+++ src/sml/SmlRequest.ts
@@ -1,64 +1,27 @@
 import SmlDocument from "./SmlDocument";
 
+console.log("Experimental > SmlRequest: This feature is a work in progress. Don't depend on it yet. It may go through significant changes.");
+
 export default class SmlRequest {
 
     public static async get(url: string): Promise<SmlDocument> {
-        const response = new Response(`Test\nEnd`, {
-            headers: {
-                "Content-Type": "text/plain"
-            },
-            status: 200
-        });
-        const responseText = await response.text();
-        return SmlDocument.parse(responseText);
+        return null;
     }
 
     public static async post(url: string, data: SmlDocument): Promise<SmlDocument> {
-        const response = await fetch(url, {
-            body: data.toString(),
-            headers: {
-                "Accept": "text/plain",
-                "Content-Type": "text/plain"
-            },
-            method: "post"
-        });
-        const responseText = await response.text();
-        return SmlDocument.parse(responseText);
+        return null;
     }
 
     public static async delete(url: string): Promise<SmlDocument> {
-        const response = await fetch(url, {
-            headers: {
-                "Content-Type": "text/plain"
-            },
-            method: "delete"
-        });
-        const responseText = await response.text();
-        return SmlDocument.parse(responseText);
+        return null;
     }
 
     public static async put(url: string, data: SmlDocument): Promise<SmlDocument> {
-        const response = await fetch(url, {
-            body: data.toString(),
-            headers: {
-                "Content-Type": "text/plain"
-            },
-            method: "put"
-        });
-        const responseText = await response.text();
-        return SmlDocument.parse(responseText);
+        return null;
     }
 
     public static async patch(url: string, data: SmlDocument): Promise<SmlDocument> {
-        const response = await fetch(url, {
-            body: data.toString(),
-            headers: {
-                "Content-Type": "text/plain"
-            },
-            method: "patch"
-        });
-        const responseText = await response.text();
-        return SmlDocument.parse(responseText);
+        return null;
     }
 
 }
